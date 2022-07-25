36ace78241eab0db5a2e5f8c90d4f6a3a36825b1
azarko <mr.azarko@gmail.com>
2022-07-25 23:44:00 +0300

    fix

--- main.py
+++ main.py
@@ -97,8 +97,12 @@ def main():
     if APP_NAME and PORT:
         # heroku host
         logging.info('Run on heroku')
-        updater.start_webhook(listen='0.0.0.0', port=int(PORT), url_path=TOKEN)
-        updater.bot.setWebhook(f'https://{APP_NAME}.herokuapp.com/{TOKEN}')
+        updater.start_webhook(
+            listen="0.0.0.0",
+            port=int(PORT),
+            url_path=TOKEN,
+            webhook_url=f'https://{APP_NAME}.herokuapp.com/{TOKEN}',
+        )
     else:
         # local host
         logging.info('Run on local host')
